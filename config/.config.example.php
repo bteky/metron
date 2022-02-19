296f59d30133d1f533aa27b42a65da80a9bb0120
atomiky <75833319+atomiky@users.noreply.github.com>
2022-02-19 21:28:40 +0800

    订阅地址 && 套餐取消季度

--- config/.config.example.php
+++ config/.config.example.php
@@ -17,7 +17,7 @@ $_ENV['version'] = 2;    //仅当涉及【需要修改config以外的文件】�
 $_ENV['key'] = '1145141919810';                //!!! 瞎 jb 修改此key为随机字符串确保网站安全 !!!
 $_ENV['debug'] = false;                          //正式环境请确保为 false
 $_ENV['appName'] = 'TK加速器';                      //站点名称
-$_ENV['baseUrl'] = 'https://oneky.xyz';               //站点地址
+$_ENV['baseUrl'] = 'https://www.oneky.xyz';               //站点地址
 $_ENV['muKey'] = 'tk_best_key';               //用于校验魔改后端请求，可以随意修改，但请保持前后端一致，否则节点不能工作！
 
 //添加主题首页
