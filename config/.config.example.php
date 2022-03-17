c8a5c5d0fcf17cad6e116f93af64cdfb1edbd440
atomiky <75833319+atomiky@users.noreply.github.com>
2022-03-17 18:44:11 +0800

    邮件

--- config/.config.example.php
+++ config/.config.example.php
@@ -53,7 +53,7 @@ $_ENV['mailgun_sender'] = '';
 # smtp
 $_ENV['smtp_host'] = 'smtp.zoho.com';                          // SMTP 邮局服务器域名
 $_ENV['smtp_username'] = 'yz@ssvipmail.xyz';                          // SMTP 账户名
-$_ENV['smtp_password'] = 'yz_Admin123';                           // SMTP 账户密码
+$_ENV['smtp_password'] = 'yz_Admin321';                           // SMTP 账户密码
 $_ENV['smtp_port'] = 465;                         // SMTP 端口(常见端口 25, 587 465)
 $_ENV['smtp_name'] = '柚子加速器';                          // SMTP 发信名称
 $_ENV['smtp_sender'] = 'yz@ssvipmail.xyz';                          // SMTP 账户发信地址
