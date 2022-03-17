4a5c5256d9c3b51db2f9870262006a47ecfe2477
atomiky <75833319+atomiky@users.noreply.github.com>
2022-03-17 19:02:42 +0800

    邮件

--- config/.config.example.php
+++ config/.config.example.php
@@ -52,18 +52,18 @@ $_ENV['mailgun_sender'] = '';
 
 # smtp
 $_ENV['smtp_host'] = 'smtp.zoho.com';                          // SMTP 邮局服务器域名
-$_ENV['smtp_username'] = 'nx@ssvipmail.xyz';                          // SMTP 账户名
-$_ENV['smtp_password'] = 'nx_Admin321';                           // SMTP 账户密码
+$_ENV['smtp_username'] = 'lingxi@oneky.xyz';                          // SMTP 账户名
+$_ENV['smtp_password'] = 'lingxi_Admin6688';                           // SMTP 账户密码
 $_ENV['smtp_port'] = 465;                         // SMTP 端口(常见端口 25, 587 465)
 $_ENV['smtp_name'] = '柚子加速器';                          // SMTP 发信名称
-$_ENV['smtp_sender'] = 'yz@ssvipmail.xyz';                          // SMTP 账户发信地址
+$_ENV['smtp_sender'] = 'lingxi@oneky.xyz';                          // SMTP 账户发信地址
 $_ENV['smtp_ssl'] = true;                        // 是否使用 TLS/SSL 发信
-$_ENV['smtp_reply_to'] = $_ENV['smtp_username'];      // 用户回复邮件的接受地址
+$_ENV['smtp_reply_to'] = $_ENV['lingxi@oneky.xyz'];      // 用户回复邮件的接受地址
 $_ENV['smtp_reply_to_name'] = $_ENV['smtp_sender'];        // 回复地址显示名
 
 # sendgrid
 $_ENV['sendgrid_key'] = '';
-$_ENV['sendgrid_sender'] = 'nx@ssvipmail.xyz';       //发件邮箱
+$_ENV['sendgrid_sender'] = 'lingxi@oneky.xyz';       //发件邮箱
 $_ENV['sendgrid_name'] = '柚子加速器';       //发件人名称
 
 # 阿里云邮件推送 WEB API
